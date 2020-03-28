e7f9e0c5df1319e86389450e422ee22dca4b097c
Ilbert Roque <36798271+ilbertroque@users.noreply.github.com>
2020-03-28 18:36:34 -0400

    Changed icons, ask the user for info, wrote setup file

--- src/watson.py
+++ src/watson.py
@@ -1,79 +1,30 @@
 #import required libraries
-import os
-import pdb
-import glob
 import json
-import shutil
-import time
+import os
 import rumps
-from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
-with open('src/data.json', 'r') as json_file:
-    data = json.load(json_file)
-
-def moveFiles():
-    folder_to_track = data['folder_to_track']
-    currentMonthYear = datetime.now().strftime('%B') + "-" + datetime.now().strftime('%y')
-
-    fileTypes = data['extensions']['documents']
-    imageTypes = data['extensions']['image']
-    videoTypes = data['extensions']['video']
-    musicTypes = data['extensions']['music']
-
-    files = retrieve(folder_to_track, fileTypes)
-    images = retrieve(folder_to_track, imageTypes)
-    videos = retrieve(folder_to_track, videoTypes)
-    music = retrieve(folder_to_track, musicTypes)
-
-    if len(files) > 0:
-        classify("Documents", files, fileTypes, currentMonthYear)
-    if len(images) > 0:
-        classify("Pictures", images, imageTypes, currentMonthYear)
-    if len(videos) > 0:
-        classify("Movies", videos, videoTypes, currentMonthYear)
-    if len(music) > 0:
-        classify("Music", music, musicTypes, currentMonthYear)
-
-def retrieve(track, extensions):
-    result = []
-    for element in extensions:
-        result.extend(glob.glob(os.path.join(track, "*" + element)))
-    return result
-
-def classify(destination, files, types, date):
-    root = data['destination_folder']
-    for file in files:
-        copying = True
-        size2 = -1
-        while copying:
-            size = os.path.getsize(file)
-            if size == size2:
-                break
-            else:
-                size2 = os.path.getsize(file)
-                time.sleep(1)
-        dir = os.path.join(root, destination, date)
-        if not os.path.exists(dir):
-            os.mkdir(dir)
-        for type in types:
-            if file.endswith(type):
-                typeDir = os.path.join(dir, type[1:].upper())
-                if not os.path.exists(typeDir):
-                    os.mkdir(typeDir)
-                shutil.move(file, typeDir)
-    rumps.notification(title="Watson", subtitle= "Just classified some files!" , message= destination + " folder updated!")
+from func import moveFiles 
 
 #Executes when folder is modified
 class MyHandler(FileSystemEventHandler):
     moveFiles()
     def on_modified(self, event):
-        moveFiles()       
+        moveFiles()
 
 class WatsonApp(object):
     def __init__(self):
-        self.app = rumps.App(data['config']['app_name'], data['config']['app_title'], data['config']['app_icon'])
+        r_file = open("data.json", "r")
+        data = json.load(r_file)
+        r_file.close()
+        self.app = rumps.App(data['config']['app_name'], data['config']['app_title'], data['config']['menu_icon'])
+        if data['destination_folder'] == "":
+            data["destination_folder"] = input("Enter path to Home folder (e.x: /Users/yourname): ")
+            data["folder_to_track"] = os.path.join(data["destination_folder"], "Downloads")
+            w_file = open("data.json", "w")
+            json.dump(data, w_file, indent=4, separators=(",", ": "), sort_keys=True)
+            w_file.close()
+
         handler = MyHandler()
         observer = Observer()
         observer.schedule(handler, data['folder_to_track'], recursive=True)
@@ -83,5 +34,6 @@ class WatsonApp(object):
         self.app.run()
 
 if __name__ == '__main__':
+    print("Application started")
     app = WatsonApp()
     app.run()  
